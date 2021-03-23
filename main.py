b6733f94efc9cf19108d9006b478d3622dbc7614
Jin <jinwu281@gmail.com>
2021-03-22 22:44:59 -0700

    implement pathfinding algorithm based on Dijkstra's algorithm

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 ORANGE = (255, 128, 0)
 PURPLE = (128, 0, 255)
-GREY = (114, 114, 114)
+GREY = (108, 108, 108)
 
-WINDOW_HEIGHT = 800
-WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 660
+WINDOW_WIDTH = 660
+INFINITY = float("inf")
 
 pygame.init()
 WINDOW = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH))
 pygame.display.set_caption("Pathfinding Visualization")
 
 grid = []
-block_size = 16
+block_size = 20
 rows = WINDOW_WIDTH // block_size
 cols = WINDOW_HEIGHT // block_size
 
@@ -33,27 +34,28 @@ class Block:
     def is_wall(self):
         return self.color == GREY
 
-    def get_color(self):
-        return self.color
-
-    def make_block(self, color):
+    def add_block(self, color):
         rect = pygame.Rect(self.row * block_size, self.col * block_size, block_size - 1, block_size - 1)
         pygame.draw.rect(WINDOW, color, rect)
         self.color = color
 
-    def update_neighbors(self):
+    def add_neighbors(self):
+
+        """append neighbor blocks (up, down, left, right) to neighbor array"""
+
         self.neighbors = []
-        if self.row < rows - 1 and not grid[self.row + 1][self.col].is_wall():
-            self.neighbors.append(grid[self.row + 1][self.col])
 
         if self.row > 0 and not grid[self.row - 1][self.col].is_wall():
             self.neighbors.append(grid[self.row - 1][self.col])
 
-        if self.col < cols - 1 and not grid[self.row][self.col + 1].is_wall():
-            self.neighbors.append(grid[self.row][self.col + 1])
+        if self.row < rows - 1 and not grid[self.row + 1][self.col].is_wall():
+            self.neighbors.append(grid[self.row + 1][self.col])
 
         if self.col > 0 and not grid[self.row][self.col - 1].is_wall():
             self.neighbors.append(grid[self.row][self.col - 1])
+
+        if self.col < cols - 1 and not grid[self.row][self.col + 1].is_wall():
+            self.neighbors.append(grid[self.row][self.col + 1])
 
         return self.neighbors
 
@@ -72,7 +74,7 @@ def draw_block():
     for i in range(rows):
         for j in range(cols):
             block = grid[i][j]
-            block.make_block(WHITE)
+            block.add_block(WHITE)
     pygame.display.update()
 
 
@@ -85,9 +87,54 @@ def clicked_pos(pos):
     return row, col
 
 
+def dijkstra(start, end):
+    unvisited = [node for row in grid for node in row]
+    distance = {node: INFINITY for row in grid for node in row}
+    path = {}
+    distance[start] = 0
+    current = None
+
+    while current != end:
+        minimum = INFINITY
+
+        for node in unvisited:
+            if distance[node] < minimum:
+                minimum = distance[node]
+
+        for node in unvisited:
+            if distance[node] == minimum:
+                current = node
+                unvisited.remove(current)
+                break
+
+        if current == end:
+            construct_path(path, current)
+            start.add_block(PURPLE)
+            return True
+
+        for neighbor in current.neighbors:
+            if neighbor.is_wall():
+                alt = INFINITY + distance[current]
+            else:
+                alt = 1 + distance[current]
+            if alt < distance[neighbor]:
+                distance[neighbor] = alt
+                path[neighbor] = current
+
+        current.add_block(GREEN)
+        pygame.display.update()
+
+
+def construct_path(path, current):
+    while current in path:
+        current = path[current]
+        current.add_block(BLUE)
+
+
 def main():
     draw_block()
     start, end = None, None
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -100,28 +147,36 @@ def main():
                 block = grid[row][col]
                 if not start and block != end:
                     start = block
-                    start.make_block(PURPLE)
+                    start.add_block(PURPLE)
                 elif not end and block != start:
                     end = block
-                    end.make_block(ORANGE)
+                    end.add_block(ORANGE)
                 elif block != start and block != end:
-                    block.make_block(GREY)
+                    block.add_block(GREY)
 
             elif pygame.mouse.get_pressed()[2]:  # Right Mouse Click
                 mouse_pos = pygame.mouse.get_pos()
                 row, col = clicked_pos(mouse_pos)
                 block = grid[row][col]
-                block.make_block(WHITE)
+                block.add_block(WHITE)
                 if block == start:
                     start = None
                 elif block == end:
                     end = None
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and start and end:
+                if event.key == pygame.K_RETURN and start and end:
                     for row in grid:
                         for block in row:
-                            block.update_neighbors()
+                            block.add_neighbors()
+
+                    dijkstra(start, end)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    draw_block()
 
         pygame.display.update()
 
